c782de6df22b3bc583b8655769e7f3ea3c4f5c42
Mega61 <73146750+Mega61@users.noreply.github.com>
2023-11-02 17:47:42 -0500

    Internet script documentation

--- internet_script.py
+++ internet_script.py
@@ -2,14 +2,19 @@ import serial
 import requests
 
 # Configuración de Serial
-SERIAL_PORT = 'COM3'  # Modifica esto según tu puerto serial
+SERIAL_PORT = 'COM3'  # Cambia esto según tu puerto serial
 BAUD_RATE = 9600
 
 # Configuración de ThingSpeak
-THINGSPEAK_CHANNEL_API_KEY = 'X06COBGOZ9ZZZUQC'  # Reemplaza con tu API Key
+THINGSPEAK_CHANNEL_API_KEY = 'X06COBGOZ9ZZZUQC'  # Reemplaza con tu API Key de ThingSpeak
 THINGSPEAK_URL = 'https://api.thingspeak.com/update'
 
 def send_to_thingspeak(temperature):
+    """
+    Envia la temperatura a ThingSpeak a través de una solicitud HTTP.
+
+    :param temperature: Valor de temperatura a enviar.
+    """
     payload = {
         'api_key': THINGSPEAK_CHANNEL_API_KEY,
         'field1': temperature
@@ -21,6 +26,9 @@ def send_to_thingspeak(temperature):
         print("Error enviando datos a ThingSpeak")
 
 def main():
+    """
+    Función principal que establece la comunicación serial, recibe datos, y los envía a ThingSpeak.
+    """
     with serial.Serial(SERIAL_PORT, BAUD_RATE) as ser:
         while True:
             line = ser.readline().decode('utf-8').strip()
